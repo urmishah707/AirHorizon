f8dfec641518433ec745f9de8d65b9d93e0900d2
urmishah3 <urmishah3006@gmail.com>
2026-05-03 20:22:46 -0400

    update

--- init/index.js
+++ init/index.js
@@ -1,6 +1,43 @@
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
+require("dotenv").config({ path: require("path").resolve(__dirname, "../.env") });
+
+const MONGO_URL = process.env.ATLASDB_URL; 
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  const dataWithGeometry = initData.data.map((obj) => ({
+    ...obj,
+    owner: "000000000000000000000000", 
+    geometry: {
+      type: "Point",
+      coordinates: [0, 0], 
+    },
+  }));
+  await Listing.insertMany(dataWithGeometry);
+  console.log("data was initialized");
+};
+
+initDB();
+
+
+/*
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/airhorizon";
 
@@ -23,3 +60,4 @@ const initDB = async () => {
 };
 
 initDB();
+*/
